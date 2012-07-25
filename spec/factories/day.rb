1a1bfdc1e53e82b8e2aab7bcc06431468c475cf6
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-07-25 08:07:38 -0700

    Got the project to the point where I can run specs, though they are failing.

---
+++ spec/factories/day.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :day do
+    date Date.new(2012, 04, 19)
+  end
+end
--- spec/models/day_spec.rb
+++ spec/models/day_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe Day do
+  day = FactoryGirl.build(:day)
+
+  it "should have a valid factory" do
+    day.should be_valid
+  end
+
   it "should have a title" do
-    
+    day.title.should_not raise_error
   end
 end
