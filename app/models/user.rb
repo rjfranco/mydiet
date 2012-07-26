8efb1337b49494b6451f1d36f9154505271c37b0
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-07-26 13:06:00 -0700

    Adding filter to prevent more than a single installation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  before_save :prevent_unless_first
+
+  private
+  def prevent_unless_first
+    unless User.all.empty?
+      flash[:alert] = 'Sorry, only one user per installation instance right now.'
+      return false
+    end
+  end
+
 end
