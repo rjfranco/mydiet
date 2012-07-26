994d6394a28514d0621a708fb1fb316c7e37e9d8
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-07-26 12:21:02 -0700

    removing the test I was trying, I think I may need to test this in the controller instead.

--- spec/models/meal_plan_spec.rb
+++ spec/models/meal_plan_spec.rb
@@ -1,17 +1,9 @@
 require 'spec_helper'
 
 describe MealPlan do
-  plan = FactoryGirl.build :meal_plan
-  new_plan = FactoryGirl.build :meal_plan
+  plan = FactoryGirl.build(:meal_plan)
 
   it "should have a valid factory" do
     plan.should be_valid
-  end
-
-  it "should disable previously active plans" do
-    plan.active.should eq false
-    puts "plan is #{plan}"
-    new_plan.active.should eq true
-    puts "new_plan is #{new_plan}"
   end
 end
