8980a77c3c07147c03852884c665614d70b3f373
Ramiro Jr. Franco <rjfranco@gmail.com>
2012-07-26 07:50:55 -0700

    adding default value to meal plan, and a couple of methods to hopefully let me grab the active one easily.

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,3 +1,17 @@
 class MealPlan < ActiveRecord::Base
   attr_accessible :active
+  has_many :meals
+  before_save :turn_off_previous_active_plan
+
+
+  def turn_off_previous_active_plan
+    previous_plan = MealPlan.active
+    previous_plan.active = false
+    previous_plan.save!
+  end
+
+  # Creating a way to easily get the active plan
+  def self.active
+    first :conditions => {:active => true}
+  end
 end
--- db/migrate/20120726143847_create_meal_plans.rb
+++ db/migrate/20120726143847_create_meal_plans.rb
@@ -1,7 +1,7 @@
 class CreateMealPlans < ActiveRecord::Migration
   def change
     create_table :meal_plans do |t|
-      t.boolean :active
+      t.boolean :active, :default => true
 
       t.timestamps
     end
